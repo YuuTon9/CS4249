8f8c1aa6d139d7ebe87472f380e1084a13a06e68
Deborah <deborahlow97@gmail.com>
2020-10-27 14:22:56 +0800

    Updates

---
+++ precache-manifest.9ab90166472489962152cf96a9624e07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a17d781e52fe4feda51bb55f48bcc48c",
+    "url": "/app1/index.html"
+  },
+  {
+    "revision": "f84c125accbb98fa626b",
+    "url": "/app1/static/css/main.aeb1d389.chunk.css"
+  },
+  {
+    "revision": "95d1f81df3ade6296eef",
+    "url": "/app1/static/js/2.1e769130.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/app1/static/js/2.1e769130.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f84c125accbb98fa626b",
+    "url": "/app1/static/js/main.cb307b6e.chunk.js"
+  },
+  {
+    "revision": "758055059e0bdb803dbf",
+    "url": "/app1/static/js/runtime-main.701490cb.js"
+  }
+]);
